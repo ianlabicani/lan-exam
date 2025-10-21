43de12b8b4fb71b92199c434abaf6c0284cf587e
ianlabicani <labicaniian@gmail.com>
2025-10-21 08:17:51 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'lan-exam',
+  webDir: 'www'
+};
+
+export default config;
