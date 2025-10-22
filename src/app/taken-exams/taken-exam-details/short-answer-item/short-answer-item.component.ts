8229eec8557c45e7ea73c08cda4d0c5c51872170
ianlabicani <labicaniian@gmail.com>
2025-10-22 10:27:33 +0800

    refactor to signals

--- src/app/taken-exams/taken-exam-details/short-answer-item/short-answer-item.component.ts
+++ src/app/taken-exams/taken-exam-details/short-answer-item/short-answer-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, input, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {
   IonCard,
@@ -9,17 +9,7 @@ import {
 } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { checkmarkCircleOutline, closeCircleOutline } from 'ionicons/icons';
-
-export interface Answer {
-  id: number;
-  taken_exam_id: number;
-  exam_item_id: number;
-  answer: string;
-  points_earned: number;
-  feedback: null | string;
-  created_at: Date;
-  updated_at: Date;
-}
+import { Answer } from '../taken-exam-details.page';
 
 @Component({
   selector: 'app-short-answer-item',
@@ -36,10 +26,8 @@ export interface Answer {
   ],
 })
 export class ShortAnswerItemComponent {
-  @Input() answer!: Answer;
-  @Input() index: number = 0;
-  @Input() question?: string | null;
-  @Input() maxPoints?: number | null;
+  answer = input.required<Answer>();
+  index = input.required<number>();
 
   constructor() {
     addIcons({ checkmarkCircleOutline, closeCircleOutline });
